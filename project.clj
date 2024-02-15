68a1bb3cbf2ebe25c970af5d599596daba9cc15f
Ujs74wiop6 <fabricioujs74@gmail.com>
2024-02-15 03:01:51 -0300

    feat: init repo

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject crud "0.1.0-SNAPSHOT"
   :description "FIXME: Simple CRUD project using Clojure"
   :url "http://localhost:3000"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "GPL-3.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.gnu.org/licenses/gpl-3.0.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [ring "1.7.1"]
                  [cheshire "5.10.0"]
